59543a70c94f090f0f66f8c8d79934b79ae640f8
Tomer <gmtomer@gmail.com>
2016-02-10 17:13:21 +0200

    logs you in and shows your name :grin:

--- app/app.jsx
+++ app/app.jsx
@@ -3,15 +3,28 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 import chatApp from './redux/reducers';
+import { signin } from './redux/actions';
+import LoginInput from './shared/login-input';
 
 const client = require('socket.io-client')('http://localhost:8080');
 let store = createStore(chatApp);
 
 class Chat extends React.Component {
+    login(username) {
+        this.props.dispatch(signin(username));
+        client.emit('add user', username);
+    }
     render() {
         return (
             <div>
-                {this.props.username ? this.props.username : "Choose a Username"}
+                {this.props.username ?
+                    <h1>Hello, {this.props.username}</h1>
+                        :
+                    <div>
+                        <h1>Choose a Username</h1>
+                        <LoginInput signin={this.login.bind(this)} />
+                    </div>
+                }
             </div>
         );
     }
