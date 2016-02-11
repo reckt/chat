79de33f61a5a347fcaddb497e69a2398d21c37a5
Tomer <gmtomer@gmail.com>
2016-02-11 11:39:31 +0200

    chat bubbles

--- app/shared/chat/chat-message.jsx
+++ app/shared/chat/chat-message.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 export default class Message extends React.Component {
     static propTypes = {
         message: React.PropTypes.string.isRequired,
+        me: React.PropTypes.bool.isRequired,
         username: React.PropTypes.string
     };
     render() {
-        return (<p>{this.props.username ? this.props.username + ": " : ""}{this.props.message}</p>);
+        return (
+            <div className={this.props.me ? "bubble me" : "bubble other"}>
+                {this.props.username ? this.props.username + ": " : ""}{this.props.message}
+            </div>
+        );
     }
 }
